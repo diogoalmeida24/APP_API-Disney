28440ac307df28cc32319756d1c3dad5f150e665
diogoalmeida24 <almeidasdiogo@gmail.com>
2022-11-06 00:21:23 -0300

    Aplicação API - Disney

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const request = async(callback) => {
+  const response = await fetch('https://api.disneyapi.dev/characters');
+  const parsed = await response.json();
+  callback(parsed.data);
+}
 
 export default function App() {
+    const [registro, setRegistro] =useState([]);
+    useEffect(() =>{request(setRegistro);},[]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}> API Disney - Séries de TV</Text>
       <StatusBar style="auto" />
+
+      <FlatList 
+      data={registro} keyExtractor={(item)=> item.tvShows.toString()} 
+      renderItem={({item})=>
+      <Text style={styles.topicos}>
+      {item.tvShows}
+      </Text>
+      }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b7d5e5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  topicos: {
+    fontSize: 25,
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    margin: 10,
+    backgroundColor: '#000000',
+    borderRadius: 8,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  titulo: {
+    fontSize: 25,
+    marginTop: 0,
+    marginVertical: 10
+  }
 });
